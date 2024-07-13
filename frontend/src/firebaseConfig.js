a0c14b2571ea93fa9a8cb61cdb5ab8df94e4ef42
Me-Mr-Dean <birkholtzfrancoisdean@gmail.com>
2024-07-13 22:16:35 +0200

    Setup for firabase and .env plus gitignore

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,16 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+//export const analytics = getAnalytics(app);
 export const googleProvider = new GoogleAuthProvider();
