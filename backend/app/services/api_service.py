866ac76a34243bb8e817107177d3d05d17b4120e
Me-Mr-Dean <birkholtzfrancoisdean@gmail.com>
2024-07-25 00:47:37 +0200

    More vercel testing backend

--- backend/app/services/api_service.py
+++ backend/app/services/api_service.py
@@ -4,10 +4,10 @@ import requests
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 
-API_BASE_URL = os.getenv("API_BASE_URL")
-API_KEY = os.getenv("API_KEY")
+# API_BASE_URL = os.getenv("API_BASE_URL")
+# API_KEY = os.getenv("API_KEY")
 
 def search_recipes(query, cuisine=None, diet=None, intolerances=None, includeIngredients=None, excludeIngredients=None, number=10):
     url = f"https://api.spoonacular.com/recipes/complexSearch"
@@ -41,17 +41,17 @@ def get_analyzed_instructions(recipe_id):
     else:
         return {"error": "Failed to fetch recipe instructions", "status_code": response.status_code}
 
-def search_recipes_by_ingredients(ingredients, number=10, ranking=1, ignore_pantry=True):
-    url = f"{API_BASE_URL}/recipes/findByIngredients"
-    params = {
-        "ingredients": ingredients,
-        "number": number,
-        "ranking": ranking,
-        "ignorePantry": str(ignore_pantry).lower(),
-        "apiKey": API_KEY
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return jsonify({"error": "Failed to fetch recipes"}), response.status_code
+# def search_recipes_by_ingredients(ingredients, number=10, ranking=1, ignore_pantry=True):
+#     url = f"{API_BASE_URL}/recipes/findByIngredients"
+#     params = {
+#         "ingredients": ingredients,
+#         "number": number,
+#         "ranking": ranking,
+#         "ignorePantry": str(ignore_pantry).lower(),
+#         "apiKey": API_KEY
+#     }
+#     response = requests.get(url, params=params)
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         return jsonify({"error": "Failed to fetch recipes"}), response.status_code
