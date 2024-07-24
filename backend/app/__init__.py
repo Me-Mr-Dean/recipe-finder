1c20a0f6df7371dc1d77a8013845789dd4f91b2b
Me-Mr-Dean <birkholtzfrancoisdean@gmail.com>
2024-07-25 01:38:18 +0200

    Added prints to check what is happening

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,15 +6,15 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dean_git_gud:fruitjuiceblend13@localhost/recipe_finder'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #vra chat
-    db.init_app(app)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dean_git_gud:fruitjuiceblend13@localhost/recipe_finder'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #vra chat
+    # db.init_app(app)
     CORS(app)
 
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    with app.app_context():
-        db.create_all()
-
+    # with app.app_context():
+    #     db.create_all()
+    print("create_app function success")
     return app
--- backend/run.py
+++ backend/run.py
@@ -4,5 +4,5 @@ from app.services.api_service import search_recipes
 app = create_app()
 
 if __name__ == '__main__':
-    # print(search_recipes("pasta", number=5))
-    app.run(debug=True)
+    print("App running!")
+    app.run()
